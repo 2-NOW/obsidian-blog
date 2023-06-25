464f3935c908d054f1c842cece7b1276d2473801
2-NOW <kj109888@gmail.com>
2023-06-26 01:17:28 +0900

    Refactor social media icons into separate component in Footer

--- components/global/layout/footer.tsx
+++ components/global/layout/footer.tsx
@@ -1,26 +1,4 @@
-import Link from "next/link";
-import { FaGithubAlt, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-
-const Socials = () => {
-  return (
-    <div className="flex items-center space-x-4">
-      {[
-        { href: "/twitter", Icon: FaTwitter, label: "Twitter" },
-        { href: "/linkedin", Icon: FaLinkedinIn, label: "LinkedIn" },
-        { href: "/github", Icon: FaGithubAlt, label: "Github" },
-      ].map(({ href, Icon }) => (
-        <Link
-          key={`social-${href}`}
-          className="text-gray-400 hover:text-gray-500"
-          href={href}
-        >
-          <span className="sr-only">{Icon.name}</span>
-          <Icon className="h-6 w-6" aria-hidden="true" />
-        </Link>
-      ))}
-    </div>
-  );
-};
+import { Socials } from "../socials";
 
 export const Footer = () => {
   return (
@@ -28,7 +6,7 @@ export const Footer = () => {
       <div className="border-t border-gray-200" />
       <div className="flex justify-between py-4">
         <div>© 2023 2-NOW</div>
-        <Socials />
+        <Socials iconSize="20px" />
       </div>
     </footer>
   );
